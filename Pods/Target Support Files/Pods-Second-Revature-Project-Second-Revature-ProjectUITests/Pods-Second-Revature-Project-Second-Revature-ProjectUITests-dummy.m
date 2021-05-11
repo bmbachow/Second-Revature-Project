f61ccc140a811a042dfd89c8c6a982f6bd5ed3ea
Kangseok Lee <kangseok0102@gmail.com>
2021-05-11 00:27:28 -0400

    Rough UI design added

---
+++ Pods/Target Support Files/Pods-Second-Revature-Project-Second-Revature-ProjectUITests/Pods-Second-Revature-Project-Second-Revature-ProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Second_Revature_Project_Second_Revature_ProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Second_Revature_Project_Second_Revature_ProjectUITests
+@end
---
+++ Pods/Target Support Files/Pods-Second-Revature-ProjectTests/Pods-Second-Revature-ProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Second_Revature_ProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_Second_Revature_ProjectTests
+@end
